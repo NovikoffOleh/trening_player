cba2290b75c9cbee2ebd9cb89c2809b40c5c038b
NovikoffOleh <novikoff10000@gmail.com>
2023-01-20 19:29:24 +0200

    Update js.js

--- js.js
+++ js.js
@@ -5,3 +5,23 @@ document.querySelector('#speed-up').onclick = speedUp;
 document.querySelector('#speed-down').onclick = speedDown;
 document.querySelector('#speed-normal').onclick = speedNormal;
 document.querySelector('#volume').onclick = videoVolume;
+
+let video;
+let disply;
+
+video = document.querySelector('#video-player');
+
+function play(){
+    video.play();
+}
+function pause(){
+    video.pause();
+}
+function stop(){
+    video.pause();
+    video.currentTime = 0;
+}
+function speedUp(){}
+function speedDown(){}
+function speedNormal(){}
+function videoVolume(){}
