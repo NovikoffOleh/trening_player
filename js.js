c449641f47ed5d67cb5299773ed84fa56f114d5f
NovikoffOleh <novikoff10000@gmail.com>
2023-01-20 22:41:48 +0200

    upd files

--- js.js
+++ js.js
@@ -4,12 +4,17 @@ document.querySelector('#stop').onclick = stop;
 document.querySelector('#speed-up').onclick = speedUp;
 document.querySelector('#speed-down').onclick = speedDown;
 document.querySelector('#speed-normal').onclick = speedNormal;
-document.querySelector('#volume').onclick = videoVolume;
+document.querySelector('#volume').oninput = videoVolume;
 
 let video;
 let disply;
+let progress; 
 
 video = document.querySelector('#video-player');
+progress = document.querySelector('#progress');
+//!
+video.ontimeupdate = progressUpdate;
+progress.onclick = videoRewind;
 
 function play(){
     video.play();
@@ -21,7 +26,33 @@ function stop(){
     video.pause();
     video.currentTime = 0;
 }
-function speedUp(){}
-function speedDown(){}
-function speedNormal(){}
-function videoVolume(){}
+function speedUp(){
+    video.play();
+    video.playbackRate = 3;
+}
+function speedDown(){
+    video.play();
+    video.playbackRate = 0.5;
+}
+function speedNormal(){
+    video.play();
+    video.playbackRate = 1;
+}
+function videoVolume(){
+    let v  = this.value;
+    console.log(v);
+    video.volume = v/100;
+}
+//бегущая полоса
+function progressUpdate(){
+console.log(video.duration);
+console.log(video.currentTime);
+let d = video.duration;
+let c = video.currentTime;
+progress.value = (100*c)/d;
+}
+//перемотка
+function videoRewind(){
+let w = this.offsetWidth;
+console.log(w);
+}
