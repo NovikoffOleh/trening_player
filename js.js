6f4d534c8ec0a8217b160003fcc42ecabfa29b9f
NovikoffOleh <novikoff10000@gmail.com>
2023-01-20 19:16:16 +0200

    correct

---
+++ js.js
@@ -0,0 +1,7 @@
+document.querySelector('#play').onclick = play;
+document.querySelector('#pause').onclick = pause;
+document.querySelector('#stop').onclick = stop;
+document.querySelector('#speed-up').onclick = speedUp;
+document.querySelector('#speed-down').onclick = speedDown;
+document.querySelector('#speed-normal').onclick = speedNormal;
+document.querySelector('#volume').onclick = videoVolume;
